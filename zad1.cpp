2ab5532ede1d1b99104e548c6872d13b9030e68d
Kantar21rus <31552264+Kantar21rus@users.noreply.github.com>
2021-05-15 21:23:20 +0300

    добавил деление на размер элемента

--- zad1.cpp
+++ zad1.cpp
@@ -6,8 +6,8 @@ int main() {
   bool middle[3];
   bool big[4];
 
-  std::cout << "Tiny:\t" << sizeof(tiny) << "\n";
-  std::cout << "Small:\t" << sizeof(small) << "\n";
-  std::cout << "Middle:\t" << sizeof(middle) << "\n";
-  std::cout << "Big:\t" << sizeof(big) << "\n";
+  std::cout << "Tiny:\t" << sizeof(tiny)/sizeof(bool)  << "\n";
+  std::cout << "Small:\t" << sizeof(small)/sizeof(bool) << "\n";
+  std::cout << "Middle:\t" << sizeof(middle)/sizeof(bool) << "\n";
+  std::cout << "Big:\t" << sizeof(big)/sizeof(bool) << "\n";
 }

10c014de770b9541283088ebabee6bf3b73821bd
Kantar21rus <31552264+Kantar21rus@users.noreply.github.com>
2021-07-16 21:57:56 +0300

    Made changes

--- zad3.cpp
+++ zad3.cpp
@@ -3,21 +3,26 @@
 
 int main() {
   std::vector<int> data(20);
+  int pos = 0;
   for (;;) {
     int input;
     std::cin >> input;
     if (input == -1) {
       std::cout << "---------------\n";
-      for(int i = 0; i < 20 ; ++i) {
+      for(int i = pos; i < data.size() ; ++i) {
+	std::cout << data[i] << "\t";
+      }
+      for(int i = 0; i < pos ; ++i) {
 	std::cout << data[i] << "\t";
       }
       std::cout << "\n";
       return 0;
     }
-    for(int i=0; i < 19; ++i ) {
-      data[i] = data[i+1];
+    data[pos] = input;
+    ++pos;
+    if (pos == 20) {
+      pos =0;
     }
-    data[19] = input;
   }
 }
     

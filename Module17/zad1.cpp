19e61261dca2b4e4b08835ef985262ffdbd1d374
Kantar21rus <31552264+Kantar21rus@users.noreply.github.com>
2021-07-30 20:58:56 +0300

    Create zad1.cpp

---
+++ Module17/zad1.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    swap(&a, &b);
+    printf("%d %d\n",a ,b);
+}

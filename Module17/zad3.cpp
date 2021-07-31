52570fe40176de024f25d2d711570ae3dc95f402
Kantar21rus <31552264+Kantar21rus@users.noreply.github.com>
2021-07-31 18:49:30 +0300

    Add files via upload

--- Module17/zad3.cpp
+++ Module17/zad3.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <cstring>
 
-bool substr (char* full, char* part, int size) 
+bool substr (char* full, char* part) 
 {
     for (int i=0; *(full+i); ++i)
     {
         if ((*(full+i)) == (*part))
         {
-            int count=0;
             for(int j=0;*(part+j); ++j)
             {
-                if (*(full+i+j)==*(part+j))
-                    ++count;
+                if ((*(full+i+j)==*(part+j)) and *(part+j+1)=='\0')
+                    return true;
             }
-            if (count == size)
-                return true;
         }
     }
     return false;
@@ -22,8 +19,8 @@ bool substr (char* full, char* part, int size)
 
 int main() {
     char* a= "Hello world";
-    char* b= "ld";
-    char* c= "olo";
-    std::cout<< (substr(a, b, strlen(b))? "true" : "false") << " " 
-        << (substr(a, c, strlen(c))? "true" : "false") << "\n";
+    char* b= "Hel";
+    char* c= "o ww";
+    std::cout<< (substr(a, b)? "true" : "false") << " " 
+        << (substr(a, c)? "true" : "false") << "\n";
 }

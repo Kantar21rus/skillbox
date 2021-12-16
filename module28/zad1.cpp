fca118618fd6b655b75104278466e7ff3b869228
Kantar21rus <31552264+Kantar21rus@users.noreply.github.com>
2021-12-16 22:13:50 +0300

    Update zad1.cpp

--- module28/zad1.cpp
+++ module28/zad1.cpp
@@ -14,23 +14,29 @@ class Swimmer
     std::time_t finishTime = 0;
     double remained = 100;
 
-
-    Swimmer(std::string &in_name, double &in_speed)
+    Swimmer()
     {
-        name = in_name;
-        speed = in_speed;
+        std::cout << "Insert name:\n";
+        std::cin >> name;
+        std::cout << "Insert his speed:\n";
+        std::cin >> speed;
     }
+    
+    static void startSwim()
+    {
+
+    }
+
 };
 
 int main()
 {
     Swimmer mySwimmer[6];
-    std::string name = "";
-    double speed = 0;
-        std::cout << "Insert name swimmer" << i << ":\n";
-        std::cin >> name;
-        std::cout << "Insert his speed:\n";
-        std::cin >> speed;
-        Swimmer(name, speed);
+    for (int i=0; i<6; ++i)
+    {
+       mySwimmer[i].startSwim();
+    } 
+
+    std::thread line1(mySwimmer[0].startSwim);
 
 }
